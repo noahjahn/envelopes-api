18ecfa2ebd5c2a87ee88ce8b8bc5e780d7dcf38d
Noah Jahn <najahn19@gmail.com>
2023-01-08 11:45:41 -0500

    Add endpoint for fetching plaid link token

--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,58 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import {
+  Configuration,
+  PlaidApi,
+  PlaidEnvironments,
+  LinkTokenCreateRequest,
+  CountryCode,
+  LinkTokenCreateRequestUser,
+  Products,
+} from 'plaid'
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.development,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': process.env.PLAID_CLIENT_ID,
+      'PLAID-SECRET': process.env.PLAID_SECRET,
+    },
+  },
+})
+
+const plaidClient = new PlaidApi(configuration)
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+Route.get('/link/token', async () => {
+  if (
+    'PLAID_CLIENT_ID' in process.env &&
+    process.env.PLAID_CLIENT_ID !== undefined &&
+    'PLAID_SECRET' in process.env
+  ) {
+    const user: LinkTokenCreateRequestUser = {
+      client_user_id: '1',
+    }
+
+    const linkTokenCreateRequest: LinkTokenCreateRequest = {
+      client_id: process.env.PLAID_CLIENT_ID,
+      secret: process.env.PLAID_SECRET,
+      client_name: 'envelopes',
+      language: 'en',
+      country_codes: [CountryCode.Us],
+      user,
+      products: [Products.Auth, Products.Transactions],
+    }
+    try {
+      const response = await plaidClient.linkTokenCreate(linkTokenCreateRequest)
+      return response.data
+    } catch (error) {
+      console.error(error)
+      return { error: error.message }
+    }
+  }
+  return { error: 'something happened' }
 })
